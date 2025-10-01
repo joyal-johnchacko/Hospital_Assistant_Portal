2d2b33b5a3d6e337c0431e3ea825f033467db372
joyal-johnchacko <ijoyaljohnchackoi@gmail.com>
2025-10-01 22:46:07 +0530

    60 percent

--- backend/hospital-api/hospital-api/src/main/java/com/hospital/hospital_api/WebConfig.java
+++ backend/hospital-api/hospital-api/src/main/java/com/hospital/hospital_api/WebConfig.java
@@ -6,12 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // This allows all origins, methods, and headers for any path in your application.
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
+
